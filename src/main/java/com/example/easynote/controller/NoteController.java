fdca1fed3c4dee662c373693afdf8e798d429a70
khanh9507 <khanh9507@gmail.com>
2019-05-06 14:13:39 +0700

    Add comment and cross-origin to FE call api

--- src/main/java/com/example/easynote/controller/NoteController.java
+++ src/main/java/com/example/easynote/controller/NoteController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.example.easynote.exception.ResourceNotFoundException;
 import com.example.easynote.model.Note;
 import com.example.easynote.repository.NoteRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class NoteController {
